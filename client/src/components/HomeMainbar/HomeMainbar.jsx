de7a9bc323e2e57c5010fcea1e20b6f5b41dbdd8
Chetan Pravin Sonawane <chetanps131@gmail.com>
2023-09-29 16:06:08 +0530

    Ask Question

--- client/src/components/HomeMainbar/HomeMainbar.jsx
+++ client/src/components/HomeMainbar/HomeMainbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import {Link, useLocation} from 'react-router-dom'
+import { useLocation, useNavigate} from 'react-router-dom'
 
 import './HomeMainbar.css'
 import QuestionList from './QuestionList'
 
 const HomeMainbar = () => {
 
-    
     const location=useLocation();
+    const user = 1;
+    const navigate = useNavigate();
+    
     var questionsList = [{
         id:1,
         votes:3,
@@ -37,13 +39,26 @@ const HomeMainbar = () => {
         askedOn:"jan 1"
     }]
 
+    
+
+   
+
+    const checkAuth = () => {
+        if(user === null){
+            alert("login or signup to ask a question")
+            navigate('/Auth')
+        } else{
+            navigate('/AskQuestion')
+        }
+    }
+
   return (
     <div className='main-bar'> 
     <div className='main-bar-header'>
         {
             location.pathname === '/' ? <h1>Top Questions</h1> : <h1>All Questions</h1>
         }
-        <Link to='/AskQuestion' className='ask-btn'>Ask Question</Link>
+        <button onClick={checkAuth} className='ask-btn'>Ask Question</button>
 
     </div>
     <div >
@@ -54,14 +69,7 @@ const HomeMainbar = () => {
                 <p>{questionsList.length} questions</p>
                 <QuestionList questionsList={questionsList} />
                
-                {/* <>
-                    {
-                        questionsList.map((question) => (
-                            <Questions question={question} key={question.id} />
-                        )      )
-                    }
-                </> */}
-            
+                
             </>
         }
     </div>
