f5bcd3c435db7d613c5d982e906b23761dd90d78
Chetan Pravin Sonawane <chetanps131@gmail.com>
2023-11-27 14:34:00 +0530

    updated Users.css

--- client/src/pages/Users/Users.jsx
+++ client/src/pages/Users/Users.jsx
@@ -10,8 +10,10 @@ const Users = () => {
     
   return (
     <div className="home-container-1">
-      <LeftSidebar  />
+      <LeftSidebar  /> 
+      
       <div className="home-container-2" style={{ marginTop: "30px" }}>
+      <h1>Users</h1>
         {
             location.pathname === '/Users' ? 
             <UsersList /> : 
